ff7f840af69fd72e3b237494019b7782da629d33
terumasa4649 <146915725+teru4649@users.noreply.github.com>
2026-09-14 23:25:49 +0900

    Update 4_モンテカルロ.py

--- pages/4_モンテカルロ.py
+++ pages/4_モンテカルロ.py
@@ -109,7 +109,12 @@ if st.button("この内容を保存"):
 st.divider()
 
 st.write("シミュレーション実行直前の distribution / param1〜3:")
-st.write(working_df.loc[leaf_mask, ["label", "distribution", "param1", "param2", "param3"]])
+st.write(
+    working_df.loc[
+        leaf_mask,
+        ["label", "worst_value", "best_value", "distribution", "param1", "param2", "param3"],
+    ]
+)
 
 n_trials = st.slider("試行回数", min_value=1000, max_value=50000, value=10000, step=1000)
 
